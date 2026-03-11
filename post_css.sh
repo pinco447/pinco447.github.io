cd839782823baefdd88c0794aab56bd8b17f38ac
Vladislav Borisenko <vladislav.borisenko@websolutionsnyc.com>
2026-03-11 15:13:00 +0300

    init

---
+++ post_css.sh
@@ -0,0 +1,2 @@
+purgecss --css public/files/css/all.css --content index.html --output public/files/css/
+purgecss --css public/files/css/custom-wrcb-35.css --content index.html --output public/files/css/
